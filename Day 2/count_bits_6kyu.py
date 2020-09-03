e857220dd2ac8753b42c3f991336ba618c4d1424
Gautham Prakash <gauthamp10@gmail.com>
2020-09-03 11:35:57 +0530

    Add count_bits_6kyu.py - done

---
+++ Day 2/count_bits_6kyu.py
@@ -0,0 +1,19 @@
+"""Write a function that takes an integer as input, and returns the number of bits
+that are equal to one in the binary representation of that number.You can guarantee
+that input is non-negative."""
+
+def count_bits(num):
+    """Function to count the number of 1's in binary of given value for n"""
+    count = ''
+    return len([count for bit in bin(num)[2:] if int(bit) == 1])
+
+def test_cases():
+    """Some useful test cases for the problem"""
+    assert count_bits(0) == 0
+    assert count_bits(4) == 1
+    assert count_bits(7) == 3
+    assert count_bits(9) == 2
+    assert count_bits(10) == 2
+    print("Test success!")
+
+test_cases()
