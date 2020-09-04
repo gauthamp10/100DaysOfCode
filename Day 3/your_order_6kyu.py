25cc8c9600d9887b3c17f2e89b8857850e7e80cc
Gautham Prakash <gauthamp10@gmail.com>
2020-09-04 10:54:47 +0530

    Update your_order_6kyu.py - done

--- Day 3/your_order_6kyu.py
+++ Day 3/your_order_6kyu.py
@@ -4,14 +4,18 @@ Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
 If the input string is empty, return an empty string.
 The words in the input String will only contain valid consecutive numbers."""
 
+import re
 def order(sentence):
     """Funciton to return a sentence in special ordering"""
-    return
+    word_dict = {re.search(r'[0-9]', word).group(0): word for word in sentence.split()}
+    return " ".join([sorted(word_dict.items())[i][1] for i in range(len(word_dict)) ])
+
 
 def test_cases():
     """Some test cases to validate"""
     assert order("is2 Thi1s T4est 3a") == "Thi1s is2 3a T4est"
     assert order("4of Fo1r pe6ople g3ood th5e the2") == "Fo1r the2 g3ood 4of th5e pe6ople"
     assert order("") == ""
+    print("Test Success!")
 
 test_cases()
