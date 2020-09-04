27583c500e4af815371a09173c91cd2f06c1f6db
Gautham Prakash <gauthamp10@gmail.com>
2020-09-04 09:16:03 +0530

    Update test_123_7kyu.py -done

--- Day 3/test_123_7kyu.py
+++ Day 3/test_123_7kyu.py
@@ -2,14 +2,16 @@
 the line numbering. Write a function which takes a list of strings and returns each line
 prepended by the correct number.The numbering starts at 1. The format is n: string.
 Notice the colon and space in between."""
-
+from itertools import cycle
 def number(lines):
     """Function to return a formatted sting with line number"""
-    pass
+    nums = cycle(range(1,len(lines)+1))
+    return [str(next(nums))+": "+line for line in lines]
 
 def test_cases():
     """Some useful test cases"""
-    assert number([]), []
+    assert number([]) == []
     assert number(["a", "b", "c"]) == ["1: a", "2: b", "3: c"]
+    print("Test Success!")
 
 test_cases()
